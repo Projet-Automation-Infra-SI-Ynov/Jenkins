3b7ddaf20614869199e5bc71c34b2104b6e5ff37
Corentin Beuf <corentin.beuf@ynov.com>
2022-04-16 18:27:47 +0200

    Add stage for add registry IP adress to daemon.json file

--- conf/jobs/registry-pipeline.groovy
+++ conf/jobs/registry-pipeline.groovy
@@ -15,7 +15,12 @@ pipeline {
                 git branch: "${params.BRANCH}", url: 'https://github.com/Projet-Automation-Infra-SI-Ynov/Registry'
             }
         }
-        stage('Add Grafana address IP') {
+        stage('Add Registry address IP in ansible role') {
+            steps {
+                sh "sed -i 's/REGISTRY2/${params.REGISTRY_IP}/g' ./Ansible/Grafana/tasks/main.yml"
+            }
+        }
+        stage('Add Registry address IP in inventory file') {
             steps {
                 sh "sed -i 's/IP_REGISTRY/${params.REGISTRY_IP}/g' ./ansible/registry.ini"
             }
